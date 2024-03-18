5d6ba1fcc4ca4c26e11e4175c40f9cccf626a060
Christopher Bailey <cbailey@mort.is>
2024-03-18 18:14:51 -0400

    Initial public client

--- span_panel/client/api_response.py
+++ span_panel/client/api_response.py
@@ -14,21 +14,15 @@ class ApiResponse:
 
     status_code: Optional[StrictInt] = Field(None, description="HTTP status code")
     headers: Optional[Dict[StrictStr, StrictStr]] = Field(
-        None,
-        description="HTTP headers",
+        None, description="HTTP headers"
     )
     data: Optional[Any] = Field(
-        None,
-        description="Deserialized data given the data type",
+        None, description="Deserialized data given the data type"
     )
     raw_data: Optional[Any] = Field(None, description="Raw data (HTTP response body)")
 
     def __init__(
-        self,
-        status_code=None,
-        headers=None,
-        data=None,
-        raw_data=None,
+        self, status_code=None, headers=None, data=None, raw_data=None
     ) -> None:
         self.status_code = status_code
         self.headers = headers
